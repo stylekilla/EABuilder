5e0dd24c18bf1b4d25456a12ed98957802e3bfc2
Micah Barnes <micah.jamiel@gmail.com>
2022-08-29 11:30:09 +1000

    Finds VAAPI libraries and includes them in the EAAPI libraries.

--- modules/FindEAAPI.cmake
+++ modules/FindEAAPI.cmake
@@ -16,31 +16,25 @@
 find_package(PkgConfig)
 set(CMAKE_FIND_LIBRARY_SUFFIXES ".lib" ".dll")
 
-# Find the EAAPI headers.
-find_path(EAAPI_INCLUDE_DIR include HINTS C:/EAAPI)
-set(EAAPI_INCLUDE_DIRS ${EAAPI_INCLUDE_DIR}/include)
+# Find the headers.
+find_path(EAAPI_INCLUDE_DIR EAAPI/Servant.h HINTS C:/EAAPI PATH_SUFFIXES include)
+set(EAAPI_INCLUDE_DIRS ${EAAPI_INCLUDE_DIR})
 
-# Find the EAAPI libraries, both Debug and Release
+# Find the Debug and Release libraries.
 find_library(EAAPI_LIBRARY NAMES EAAPI_x64_Release.lib HINTS C:/EAAPI/lib/x64_Release)
-find_library(EAAPI_DEBUG_LIBRARY NAMES EAAPI_x64_Debug.lib HINTS C:/EAAPI/lib/x64_Debug)
-set(EAAPI_LIBRARIES optimized ${EAAPI_LIBRARY} Rpcrt4.lib
-                    debug     ${EAAPI_DEBUG_LIBRARY} Rpcrt4.lib)
-					
-# Find the VAAPI libraries, both Debug and Release
 find_library(VAAPI_LIBRARY NAMES VAAPI.lib HINTS C:/EAAPI/lib/x64_Release)
+find_library(EAAPI_DEBUG_LIBRARY NAMES EAAPI_x64_Debug.lib HINTS C:/EAAPI/lib/x64_Debug)
 find_library(VAAPI_DEBUG_LIBRARY NAMES VAAPI.lib HINTS C:/EAAPI/lib/x64_Debug)
-set(VAAPI_LIBRARIES optimized ${VAAPI_LIBRARY} Rpcrt4.lib
-                    debug     ${VAAPI_DEBUG_LIBRARY} Rpcrt4.lib)
+set(EAAPI_LIBRARIES optimized ${EAAPI_LIBRARY} ${VAAPI_LIBRARY} Rpcrt4.lib
+                    debug     ${EAAPI_DEBUG_LIBRARY} ${VAAPI_DEBUG_LIBRARY} Rpcrt4.lib)
 
 # Find the EAAPI binaries, both Debug and Release
 find_library(EAAPI_BINARY NAMES VCServant.dll HINTS C:/EAAPI/bin/x64_Release)
 find_library(EAAPI_DEBUG_BINARY NAMES VCServantd.dll HINTS C:/EAAPI/bin/x64_Debug)
 set(EAAPI_BINARIES ${EAAPI_BINARY} ${EAAPI_DEBUG_BINARY})
 
-
 # Set compiler flags for both Debug and Release target
 set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} /MD /EHsc /Zp8 /D _SECURE_SCL=0 /D NDEBUG")
 set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} /MDd /EHsc /Zp8 /D _SECURE_SCL=0 /D _HAS_ITERATOR_DEBUGGING=0")
 
-find_package_handle_standard_args(EAAPI DEFAULT_MSG EAAPI_LIBRARY EAAPI_INCLUDE_DIR)
-
+find_package_handle_standard_args(EAAPI DEFAULT_MSG EAAPI_LIBRARY VAAPI_LIBRARY EAAPI_INCLUDE_DIR)
